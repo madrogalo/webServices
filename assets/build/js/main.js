042e86f11a7fb4dc1ebeecd5bffbf45613bc10d1
ievhenii <youn4ik@gmail.com>
2019-05-13 22:03:24 +0300

    create

--- assets/build/js/main.js
+++ assets/build/js/main.js
@@ -17926,3 +17926,20 @@ return Popper;
 //# sourceMappingURL=popover.js.map
 
 // Импортируем другие js-файлы
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("heder-show-hide").style.top = "0";
+  } else {
+    document.getElementById("heder-show-hide").style.top = "-95px";
+    document.getElementById("show-mobile-menu").classList.remove('active-nemu');
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+// show hide nav
+function clickShowAndHideMenu() {
+  document.getElementById('show-mobile-menu').classList.toggle('active-nemu');
+}
